167aee445c853c637b27d0db820d5c1bfe0f662c
3dhardgamer <artur.turbas@gmail.com>
2017-08-15 00:13:21 -0300

    Rename app.py to main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+'''
+Main application:
+
+    - Before running this program make sure to run db_creation.py first.
+      In case you have already done it, you are good to go! :)
+
+    - Pyramid app is created:
+        - routes are defined
+        - views are called from views.py
+        - server is kept running
+'''
+
 from wsgiref.simple_server import make_server
+from pyramid.session import SignedCookieSessionFactory
 from pyramid.config import Configurator
 
 if __name__ == '__main__':
     print ('Defining routes...')
+    cookie_session = SignedCookieSessionFactory('itsagoodshow')
     with Configurator() as config:
         config.add_route('home', '/')
         config.add_route('quotes', '/quotes')
         config.add_route('pick_quote', '/quotes/{quote_number}')
+        config.add_route('log', '/log')
+        config.add_route('log_id', '/log/{log_id}')
+        config.set_session_factory(cookie_session)
         config.scan('views')
         app = config.make_wsgi_app()
     print('Starting server')
