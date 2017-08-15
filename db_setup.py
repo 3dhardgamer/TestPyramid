0548e1fdc78fdc0ec2110be083a4780d09615825
3dhardgamer <artur.turbas@gmail.com>
2017-08-15 00:34:30 -0300

    Comments added to db_setup.py

--- db_setup.py
+++ db_setup.py
@@ -1,9 +1,18 @@
+'''
+Database structure
+'''
+
 from sqlalchemy import *
 
 db = create_engine('sqlite:///app_connection.db')
 db.echo = False
 metadata = MetaData(db)
 
+# Attributes recorded:
+# request_id -> incresing counter for the number of request done to server
+# session_id -> unique session identificator
+# request_con -> path requested by the user
+# date -> date when request action happened
 request_session = Table('request_session', metadata,
                 Column('request_id', Integer, primary_key = True),
                 Column('session_id', String(32)),
