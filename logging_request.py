8178d5623c0d9173686e5735cde01d95cc54935a
3dhardgamer <artur.turbas@gmail.com>
2017-08-15 00:31:33 -0300

    Comments added to logging_request.py

--- logging_request.py
+++ logging_request.py
@@ -1,6 +1,17 @@
+'''
+Records the session information to database.
+'''
+
+
 from db_setup import *
 import datetime
 
 def logging_request(session_id, request_con):
+    '''
+    logging_request records the session information to database
+    arguments:
+        - session_id -> unique session identificator
+        - request_con -> access requested by the user 
+    '''
     log = request_session.insert()
     log.execute({'session_id': session_id, 'request_con': request_con, 'date': datetime.datetime.now()})
